892d9db13e1731dae6427458dfeed433ad750766
Romain Baghi <romain.baghi@gmail.com>
2019-04-12 14:54:42 +0200

    uncomment debug

--- atlas/atlasRoutes.py
+++ atlas/atlasRoutes.py
@@ -278,9 +278,7 @@ def ficheEspece(cd_ref):
         current_app.config["ATTR_MENACES"],
     )
     observers = vmObservationsRepository.getObservers(connection, cd_ref)
-    print('#######################################')
-    print(years)
-    print('#######################################')
+    
     connection.close()
     session.close()
 
